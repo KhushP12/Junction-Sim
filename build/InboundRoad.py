0153c3837d8a63b2fca0126a804717beda39db16
KhushP12 <148048822+KhushP12@users.noreply.github.com>
2025-02-10 13:34:06 +0000

    Update InboundRoad.py
    Added in getters/setters but just need to clarify the logic on some of the functions

--- build/InboundRoad.py
+++ build/InboundRoad.py
@@ -1,11 +1,50 @@
 class InboundRoad:
     def __init__(self):
         self.total_standard_lanes = 1
-        # Commented out till implementation
-        # self.has_bus_lane = False
-        # self.has_left_lane = False
-        # self.priority_factor = 0
+        self.has_bus_lane = False
+        self.has_left_lane = False
+        # Note each direction will have a rating from 1-4
+        # With 4 being the first priority and 1 being the least
+        # Upon initialization i've set all the priorities to 0?
+        # I think we need to talk about this
+        self.priority_factor = 0
 
-    # TODO: Getters and Setters as needed/wanted
+    # Here we return the number of lanes that the road has 
+    def total_standard_lanes(self):
+        return self.total_standard_lanes
+    
+    # Takes in the number of lanes and increments or decrements based on which button is pressed 
+    # Shouldn't return anything
+    # This should take in a signal based on which button is pressed and then increment or decrement based on which is clicked
+    def set_total_standard_lanes(self):
+        return 1
+
+    # Returns the boolean if there is a bus lane or if there isn't
+    def has_bus_lane(self):
+        return self.has_bus_lane
+
+    # Changes the flag if there is a bus lane or if there isn't
+    # has_bus should be the input if the toggle is on or not
+    def set_has_bus_lane(self, has_bus):
+        self.has_bus_lane = has_bus
+    
+    # Returns if there is a bus lane or not (Boolean)
+    def has_left_lane(self):
+        return self.has_left_lane
+    
+    # Changes the flag if there is a left lane or not
+    # Shouldn't return anything
+    # Same logic as the bus lane
+    def set_has_left_lane(self, has_left):
+        self.has_left_lane = has_left
+    
+    # Returns what the priority factor is
+    def priority_factor(self):
+        return self.priority_factor
+    
+    # Allows for the priority factor to be changed
+    def set_priority_factor(self, num :int):
+        self.priority_factor = num
+    
 
 
